a7699792e927476862968ab739561ad58033a11b
Joaquim <joaquimlima0612@gmail.com>
2026-08-30 12:04:01 -0300

    Cap1 - Dashboard de Vendas Globais

---
+++ exercicios/ex001/exigencias.py
@@ -0,0 +1,8 @@
+# Dashboard Analítico de Vendas Globais
+    # qual o valor total vendido?
+    # quantas vendas foram realizadas por categoria de produto?
+    # quantas vendas foram realizadas por país considerando a prioridade de entrega?
+    # qual foi a média de desconto nas vendas por subcategoria de produto?
+    # quais países tiveram maior média de valor de venda? demonstre em um mapa
+
+# Nosso dashboard deve dar ao usuário a possibilidade de filtrar os dados por ano, por segmento e por país
