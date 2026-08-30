67f60e8753c70ba40d60f7451d8185d248b6f8f7
Joaquim <joaquimlima0612@gmail.com>
2026-08-30 11:16:14 -0300

    Estilização do README, teoria introdutória e organização das pastas

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# power-bi-dsa
-Curso de Power BI do DSA (Data Science Academy)
+# Power BI Básico - Intermediário
+
+> Curso de Power BI do DSA (Data Science Academy)
+
+---
+
+# Etapas do Aprendizado
+
+## 1. Business Intelligence e Análise de Dados
+
+- **Primeiros Passos com Microsoft Power BI**
+- **Modelagem, Relacionamentos e DAX**
+- **Power BI para Análise de Dados de Marketing**
+- **Power BI para Análise de Dados Comerciais**
+- **Power BI para Análise de Dados de Recursos Humanos**
+- **Power BI para Análise de Dados de Logística**
+- **Power BI para Análise de Dados Financeiros**
+- **Power BI para Análise de Dados Contábeis**
+- **Power BI para Análise de Dados do Mercado de Ações**
+
+## 2. Data Science, SQL, Machine Learning, IA e Power Plataform
+
+- **Estatística Fundamental para Data Science com Power BI**
+- **Limpeza e Manipulação de Dados com Power BI**
+- **Manipulação de Dados com Power Query M Language**
+- **Power BI e Banco de Dados**
+- **SQL Analytics**
+- **Machine Learning e Power BI para Segmentação de Clientes**
+- **Machine Learning e Power BI para Detecção de Anomalias**
+- **Inteligência Artificial e Análise de Séries Temporais com Power BI**
+- **Estudo de Caso - Transformação Digital com Microsoft Power Plataform**
---
+++ teoria.py
@@ -0,0 +1,61 @@
+# O que é Business Intelligence?
+    # -> •	BI é um conjunto de técnicas e ferramentas utilizadas para transformar dados em informações úteis para a tomada de decisão empresarial. BI permite aos usuários analisar dados de diferentes fontes, criar relatórios e visualizações e monitorar indicadores-chave de desempenho (KPIs) para ajudar a identificar tendências e oportunidades. As ferramentas de BI incluem relatórios, dashboards, mineração de dados, análise estatística e até Inteligência Artificial
+
+# Principais casos de aplicação de BI
+    # -> Finanças
+        # analisar dados financeiros, como receita, despesas e lucro, para identificar tendências e prever resultados futuros. Isso pode ajudar a identificar oportunidades de economia e crescimento
+    # -> Vendas
+        # analisar dados de vendas, como volume de vendas, receita e margem de lucro, para identificar tendências e oportunidades. Isso pode ajudar a identificar produtos ou regiões que estão performando bem ou mal e tomar medidas para aumentar as vendas
+    # -> Operações
+        # monitorar indicadores-chave de desempenho (KPIs), como tempo de ciclo de produção, taxa de defeitos e disponibilidade de equipamentos, para identificar problemas e oportunidades de melhoria
+    # -> Recursos Humanos
+        # analisar dados de funcionários, como taxa de rotatividade, tempo de serviço e desempenho, para identificar tendências e oportunidades. Isso pode ajudar a identificar problemas com o clima organizacional ou com programas de treinamento e desenvolvimento
+    # -> Saúde
+        # monitorar e analisar os dados de pacientes, como histórico médico, resultados de exames e tratamentos, a fim de identificar tendências de saúde, bem como identificar problemas de saúde e fornecer informações para a tomada de decisão clínica
+
+# Business Intelligence e Big Data
+    # -> São duas áreas relacionadas. BI é um conjunto de técnicas e ferramentas usadas para transformar dados em informações valiosas para a tomada de decisões de negócios. Enquanto Big Data se refere a grandes quantidades de dados, provenientes de uma variedade de fontes e com diferentes formatos
+    # -> A capacidade de processar grandes quantidades de dados é uma das principais vantagens do Big Data, pois permite coletar e armazenar dados de muitas fontes diferentes, incluindo transações de negócios, dados de redes sociais, registros de sensores e outros tipos de dados não estruturados. BI usa esses dados para criar insights e entender tendências que podem ser usadas para melhorar a eficiência e rentabilidade dos negócios
+
+# O que é Data Science?
+    # -> •	Um campo interdisciplinar que combina matemática, estatística e computação para extrair conhecimento e insights úteis a partir de grandes conjuntos de dados
+    # -> Os profissionais de Data Science usam técnicas de análise estatística, mineração de dados, aprendizado de máquina e Inteligência Artificial para explorar e entender os dados, criar modelos para prever resultados e comunicar os insights obtidos para ajudar na tomada de decisão. Data Science é amplamente utilizada em vários setores, como finanças, saúde, marketing, varejo e tecnologia
+
+# Principais casos de aplicação de Data Science
+    # -> Marketing
+        # analisar dados de comportamento do cliente, como histórico de compras, buscas e cliques em anúncios, para segmentar melhor o público-alvo e personalizar as campanhas de marketing
+    # -> Finanças
+        # analisar dados financeiros, como transações bancárias e dados de mercado, para identificar tendências e prever resultados futuros. Isso pode ajudar a identificar oportunidades de investimento e riscos
+    # -> Vendas
+        # analisar dados de vendas, como volume de vendas e histórico de clientes, para prever a demanda e otimizar as estratégias de vendas
+    # -> Operações
+        # analisar dados de produção, como tempo de ciclo de produção e taxa de defeitos, para identificar problemas e oportunidades de melhoria
+    # -> Saúde: monitorar 
+        # analisar os dados de pacientes, como histórico médico, resultados de exames e tratamentos, para identificar tendências de saúde, bem como identificar problemas de saúde e fornecer informações para a tomada de decisão clínica
+    # -> Logística
+        # otimizar rotas de entrega, prever demanda e usar algoritmos de Machine Learning para acompanhar e prever tendências de transporte e logística
+
+# O que é o Microsoft Power BI?
+    # -> Uma plataforma da Microsoft que permite aos usuários visualizar e analisar dados de diferentes fontes, criar relatórios e dashboards interativos e compartilhar insights com outros usuários
+    # -> Permite que os usuários se conectem a diversas fontes de dados, incluindo planilhas, bancos de dados, serviços de nuvem e aplicativos, e transformem esses dados em informações valiosas
+    # -> Com o Power BI, os usuários podem importar, limpar, transformar e modelar dados, criar relatórios e visualizações e publicá-los em um ambiente compartilhado (na versão paga). Ele também tem recursos avançados como dashboards interativos, análise de dados, Inteligência Artificial e automatização de relatórios
+
+# O que é a Microsoft Power Plataform?
+    # -> Uma plataforma de automação de negócios e desenvolvimento de aplicativos baseada em nuvem que permite aos usuários criar e personalizar soluções de negócios sem a necessidade de programação de computadores
+    # -> Projetada para ser fácil de usar, escalável e segura, e é baseada na plataforma Microsoft Azure, permitindo aos usuários aproveitar a segurança, a escalabilidade e os recursos de inteligência artificial da Azure
+
+# Componentes principais da Power Plataform
+    # -> Power Automate
+        # permite aos usuários automatizar fluxos de trabalho, como aprovações, notificações e integrações com aplicativos e serviços externos, usando regras e fluxos de trabalho pré-construídos ou personalizados
+    # -> Power Apps
+        # permite aos usuários criar aplicativos personalizados para web e dispositivos móveis sem necessidade de programação, usando modelos e componentes pré-construídos ou personalizados
+    # -> Power BI
+        # permite aos usuários visualizar e analisar dados de diferentes fontes, criar relatórios e dashboards interativos e compartilhar insights com outros usuários
+    # -> Power Virtual Agents
+        # permite aos usuários criar chatbots personalizados sem necessidade de programação, usando modelos e componentes pré-construídos ou personalizados
+
+# Bibliografia
+    # -> Documentação do Power BI: https://learn.microsoft.com/pt-br/power-bi/
+    # -> Power BI Avançado Para Análise de Dados com DAX: https://www.datascienceacademy.com.br/course/power-bi-avancado-para-analise-de-dados-com-dax
+    # -> Modelagem e Análise de Dados com Power BI: https://www.datascienceacademy.com.br/course/modelagem-e-analise-de-dados-com-power-bi
+    # -> Storytelling, Dashboards e Técnicas de Apresentação Para Cientistas de Dados: https://www.datascienceacademy.com.br/course/storytelling-dashboards-e-tecnicas-de-apresentacao-para-cientistas-de-dados
